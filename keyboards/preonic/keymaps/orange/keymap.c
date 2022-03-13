1d4b310498026c03be37afe1785b6fab74977ad3
Olle Aid Kvarnström <olle+git@iix.se>
2022-03-13 16:46:23 +0100

    Switched around some keys in the special layers
    After programming a bit I quickly realized that my hands are not resting
    at asdf jkl; as they are supposed to, but instead asdf hjkl. So there
    was a misalignment issue with some keys, and some keys were very
    annoying to reach for.

--- keyboards/preonic/keymaps/orange/keymap.c
+++ keyboards/preonic/keymaps/orange/keymap.c
@@ -151,9 +151,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |   (  |   )  |   '  |   ;  |   [  |   ]  |      |      |      |
+ * |      |      |   '  |   (  |   )  |      |   [  |   ]  |   ;  |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |   -  |   \  |   /  |   =  |      |      |      |      |
+ * |      |      |      |   \  |   -  |      |   /  |   =  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -161,8 +161,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_LPRN, KC_RPRN, KC_QUOT, KC_SCLN, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_NUBS, KC_SLSH, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, KC_QUOT, KC_LPRN, KC_RPRN, XXXXXXX, KC_LBRC, KC_RBRC, KC_SCLN, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, KC_NUBS, KC_MINS, XXXXXXX, KC_SLSH, KC_EQL,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, _______, _______, _______, _______, _______, _______, ADJUST,  _______, _______, _______, _______
 ),
 
@@ -173,9 +173,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |   <  |   >  |   "  |   :  |   {  |   }  |      |      |      |
+ * |      |      |   "  |   <  |   >  |      |   {  |   }  |   :  |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |   _  |   |  |   ?  |   +  |      |      |      |      |
+ * |      |      |      |   |  |   _  |      |   ?  |   +  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
@@ -183,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  XXXXXXX, XXXXXXX, XXXXXXX, KC_LT,   KC_GT,   KC_DQUO, KC_COLN, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, KC_DQUO, KC_LT,   KC_GT,   XXXXXXX, KC_LCBR, KC_RCBR, KC_COLN, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_UNDS, KC_PIPE, KC_QUES, KC_PLUS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, _______, _______, _______, ADJUST,  _______, _______, _______, _______, _______, _______, _______
 ),
