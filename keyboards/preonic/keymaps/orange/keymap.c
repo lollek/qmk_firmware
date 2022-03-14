44bf35b89a7f4c7fe27da9a1aa169c7dd30e937f
Olle Aid Kvarnström <olle+git@iix.se>
2022-03-14 12:10:21 +0100

    Remove fallthrough from MANUAL to QWERTY
    Since we don't always want all keys to fall through, it's better to do
    it manually here I think. Also, I'm thinking of moving the MANUAL layer
    further away from QWERTY, which might cause issues if fallthroughs still
    exist

--- keyboards/preonic/keymaps/orange/keymap.c
+++ keyboards/preonic/keymaps/orange/keymap.c
@@ -104,23 +104,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Manual
  * ,-----------------------------------------------------------------------------------.
- * | ____ |Accel1|Accel2|Accel3|      |      |      |      |      |      |      | ____ |
+ * |      |Accel1|Accel2|Accel3|      |      |      |      |      |      |      | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ____ |Click1| MUp  |Click3|      |      | WLeft| WDown| WUp  |WRight|      |      |
+ * | Tab  |Click1| MUp  |Click3|      |      | WLeft| WDown| WUp  |WRight|      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | ____ | MLeft| MDown|MRight|Click2|      | Left | Down |  Up  | Right|      |      |
+ * | Esc  | MLeft| MDown|MRight|Click2|      | Left | Down |  Up  | Right|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | ____ | ____ | ____ | ____ | ____ |      | Home | PGDN | PGUP |  End |      | ____ |
+ * | Shift|   Z  |   X  |   C  |   V  |      | Home | PGDN | PGUP |  End |      | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | ____ | ____ | ____ | ____ | ____ | ____   ____ | ____ | ____ | ____ | ____ |Qwerty|
+ * | Ctrl |      | GUI  | Alt  |Lower |    Space    | Raise| RAlt |      |      |Qwerty|
  * `-----------------------------------------------------------------------------------'
  */
 [_MANUAL] = LAYOUT_preonic_grid(
-  XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-  _______, KC_BTN1, KC_MS_U, KC_BTN3, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,
-  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXXXXX, XXXXXXX,
-  _______, _______, _______, _______, _______, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QWERTY
+  XXXXXXX, KC_ACL0, KC_ACL1, KC_ACL2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
+  KC_TAB,  KC_BTN1, KC_MS_U, KC_BTN3, XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D,            KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX,
+  KC_ESC,  KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2, XXXXXXX, KC_LEFT, KC_DOWN,            KC_UP,   KC_RIGHT,XXXXXXX, XXXXXXX,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    XXXXXXX, KC_HOME, KC_PGDN,            KC_PGUP, KC_END,  XXXXXXX, KC_ENT,
+  KC_LCTL, XXXXXXX, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  LT(_RAISE, KC_ENT), KC_RALT, XXXXXXX, XXXXXXX, QWERTY
 ),
 
 
