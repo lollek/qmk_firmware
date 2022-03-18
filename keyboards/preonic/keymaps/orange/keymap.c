4dae6e47ef1a611d7c1f6f36fdc5416f431cbe35
Olle Aid Kvarnström <olle+git@iix.se>
2022-03-18 08:04:56 +0100

    Flip ADJUST+LOWER + v or m

--- keyboards/preonic/keymaps/orange/keymap.c
+++ keyboards/preonic/keymaps/orange/keymap.c
@@ -179,7 +179,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |   '  |   (  |   )  |   -  |   +  |   [  |   ]  |   :  |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | ____ |      |      |      |   \  |      |      |   /  |      |      |      |      |
+ * | ____ |      |      |      |   /  |      |      |   \  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ |
  * `-----------------------------------------------------------------------------------'
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
   XXXXXXX, XXXXXXX, KC_QUOT, KC_LPRN, KC_RPRN, KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_SCLN, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_NUBS, XXXXXXX, XXXXXXX, KC_SLSH, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLSH, XXXXXXX, XXXXXXX, KC_NUBS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, _______, _______, _______, _______, _______, _______, ADJUST,  _______, _______, _______, _______
 ),
 
@@ -201,7 +201,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |   "  |   <  |   >  |   _  |   =  |   {  |   }  |   :  |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | ____ |      |      |      |   |  |      |      |   ?  |      |      |      |      |
+ * | ____ |      |      |      |   ?  |      |      |   |  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ |
  * `-----------------------------------------------------------------------------------'
@@ -210,7 +210,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
   XXXXXXX, XXXXXXX, KC_DQUO, KC_LT,   KC_GT,   KC_UNDS, KC_EQL,  KC_LCBR, KC_RCBR, KC_COLN, XXXXXXX, XXXXXXX,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE, XXXXXXX, XXXXXXX, KC_QUES, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUES, XXXXXXX, XXXXXXX, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   _______, _______, _______, _______, ADJUST,  _______, _______, _______, _______, _______, _______, _______
 ),
 
