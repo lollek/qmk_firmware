bbadf7ed3c97ab9998dce7435434f9f35853807f
Olle Aid Kvarnström <olle+git@iix.se>
2022-04-19 20:13:02 +0200

    Create SPACE1 layer
    Layer which mirrors SPACE2

--- keyboards/preonic/keymaps/orange/keymap.c
+++ keyboards/preonic/keymaps/orange/keymap.c
@@ -37,6 +37,9 @@
  * mode, etc. The key to hold down is placed in an awkward place to avoid
  * accidentally entering this layer.
  *
+ * ### SPACE1 (keydown from QWERTY)
+ * Convenience keys for navigation when writing text.
+ *
  * ### SPACE2 (keydown from QWERTY)
  * Convenience keys for navigation when writing text.
  *
@@ -67,6 +70,7 @@ enum preonic_layers {
   _QWERTY,
   _MANUAL,
   _SYSTEM,
+  _SPACE1,
   _SPACE2,
   _LOWER,
   _RAISE,
@@ -77,6 +81,7 @@ enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
   MANUAL,
   SYSTEM,
+  SPACE1,
   SPACE2,
   LOWER,
   RAISE,
@@ -96,15 +101,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   ?  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |System| GUI  | Alt  |Lower | Space|Space2| Raise| RAlt |      |      |      |
+ * | Ctrl |System| GUI  | Alt  |Lower |Space1|Space2| Raise| RAlt |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,                KC_7,                KC_8,    KC_9,    KC_0,       KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,                KC_U,                KC_I,    KC_O,    KC_P,       RALT(KC_W),
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,                KC_J,                KC_K,    KC_L,    RALT(KC_P), RALT(KC_Q),
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,                KC_M,                KC_COMM, KC_DOT,  KC_QUES,    KC_ENT,
-  KC_LCTL, SYSTEM,  KC_LGUI, KC_LALT, LOWER,  KC_SPC,  LT(_SPACE2, KC_SPC), LT(_RAISE, KC_ENT),  KC_RALT, XXXXXXX, XXXXXXX,    XXXXXXX
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                 KC_6,                KC_7,                KC_8,    KC_9,    KC_0,       KC_BSPC,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,                 KC_Y,                KC_U,                KC_I,    KC_O,    KC_P,       RALT(KC_W),
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,                 KC_H,                KC_J,                KC_K,    KC_L,    RALT(KC_P), RALT(KC_Q),
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                 KC_N,                KC_M,                KC_COMM, KC_DOT,  KC_QUES,    KC_ENT,
+  KC_LCTL, SYSTEM,  KC_LGUI, KC_LALT, LOWER,  LT(_SPACE1, KC_SPC),  LT(_SPACE2, KC_SPC), LT(_RAISE, KC_ENT),  KC_RALT, XXXXXXX, XXXXXXX,    XXXXXXX
 ),
 
 /* Manual
@@ -148,6 +153,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MPRV, KC_MNXT, MUV_IN,  MU_MOD,  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
   XXXXXXX, KC_MPLY, MUV_DE,  XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,  KC_P1,   KC_P2,   KC_P3,   KC_PENT,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,  KC_PCMM, KC_P0,   KC_PDOT, MANUAL
+),
+
+/* Space1
+ * ,-----------------------------------------------------------------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      | WLeft| WDown|  WUp |WRight|      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      | Left | Down |  Up  | Right|      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | ____ | Home | PGDN | PGUP |  End |      |      |      |      |      |      | ____ |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ | ____ |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_SPACE1] = LAYOUT_preonic_grid(
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Space2
@@ -257,6 +283,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             layer_on(_SYSTEM);
           } else {
             layer_off(_SYSTEM);
+          }
+          return false;
+          break;
+        case SPACE1:
+          if (record->event.pressed) {
+            layer_on(_SPACE1);
+          } else {
+            layer_off(_SPACE1);
           }
           return false;
           break;
